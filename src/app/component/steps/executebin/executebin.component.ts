085ce276a6827eb5f921b825ae2f420b901ffff9
Srevin A S <srevinias@gmail.com>
2022-04-16 22:10:53 +0530

    execute bin creation
    execute bin creation

--- src/app/component/steps/executebin/executebin.component.ts
+++ src/app/component/steps/executebin/executebin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterService } from 'src/app/service/router.service';
 
 @Component({
   selector: 'app-executebin',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExecutebinComponent implements OnInit {
 
-  constructor() { }
+  constructor(public routeService: RouterService) { }
 
   ngOnInit(): void {
   }
