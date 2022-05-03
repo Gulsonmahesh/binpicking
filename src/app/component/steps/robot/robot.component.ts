e2e5f2c5ac8cf09139c09b4dce003ced43ed462b
Gulson Mahesh <mahesh.madasamy@wipro.com>
2022-05-03 17:34:26 +0530

    Adding Delete model and functionality for Robot Admin

--- src/app/component/steps/robot/robot.component.ts
+++ src/app/component/steps/robot/robot.component.ts
@@ -14,6 +14,8 @@ export class RobotComponent implements OnInit {
   showRobotModel = false;
   deleteModel = false;
   showGripperModel = false;
+  selectedRobot: any = null;
+  showDeleteGripperModel = false;
 
   @HostListener('document:keyup', ['$event']) closeModel(event: KeyboardEvent) {
     if(this.showRobotModel && event.key == 'Escape') {
@@ -29,26 +31,27 @@ export class RobotComponent implements OnInit {
   
   constructor(public routeService: RouterService,private robotservice: RobotService) { }
   
-  robotDetails? = [
+
+  robotDetails = [
     {
       robotimg: 'assets/robot.webp', manufacturename: 'Universal Robot',model: 'UR56',
-      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false
+      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false, id:1
     },
     {
       robotimg: 'assets/robot.webp', manufacturename: 'Universal Robot', model: 'UR56',
-      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false
+      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false, id:2
     },
     {
       robotimg: 'assets/robot.webp', manufacturename: 'Universal Robot', model: 'UR56',
-      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false
+      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false, id:3
     },
     {
       robotimg: 'assets/robot.webp', manufacturename: 'Universal Robot', model: 'UR56',
-      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false
+      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false, id:4
     },
     {
       robotimg: 'assets/robot.webp', manufacturename: 'Universal Robot', model: 'UR56',
-      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false
+      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false, id:5
     }
   ];
 
@@ -75,6 +78,19 @@ export class RobotComponent implements OnInit {
 
   openDeleteModel(status: boolean) {
     this.deleteModel = status;
+  }
+
+  onselectedRobot(event: any) {
+    console.log(event.id)
+    this.selectedRobot = event;    
+  }
+
+  openDeleteRobot() {
+    this.showDeleteGripperModel = true;
+  }
+  onDeleteConfirm() {
+    this.robotDetails = this.robotDetails.filter((robot: any) => robot.id !== this.selectedRobot.id);    
+    this.showDeleteGripperModel = false;
   }
 
 }
