c99393b2f2d93b4464fafb9ae5ac93f4af83e2a9
Gulson Mahesh <mahesh.madasamy@wipro.com>
2022-05-19 08:48:17 +0530

    Edit Robot and Update gripper Models for Admin Robot page
    Edit Robot and Update gripper Models for Admin Robot page

--- src/app/component/steps/robot/robot.component.ts
+++ src/app/component/steps/robot/robot.component.ts
@@ -2,6 +2,7 @@ import { Component, HostListener, OnInit, ViewChild,ElementRef, Input  } from '@
 import { RouterService } from 'src/app/service/router.service';
 import { RobotService } from 'src/app/service/robot.service';
 import { RobotDetailsComponent } from '../../common/robot-details/robot-details.component';
+import { GRIPPERS } from "../../../constant";
 
 @Component({
   selector: 'app-robot',
@@ -16,7 +17,12 @@ export class RobotComponent implements OnInit {
   showGripperModel = false;
   selectedRobot: any = null;
   showDeleteGripperModel = false;
-
+  showSelectGripper = false;
+  GRIPPERS = GRIPPERS;
+  robotId: any;
+  isEdit = false;
+  selectedGripperId: any;
+  
   @HostListener('document:keyup', ['$event']) closeModel(event: KeyboardEvent) {
     if(this.showRobotModel && event.key == 'Escape') {
       this.showRobotModel = false;
@@ -34,30 +40,33 @@ export class RobotComponent implements OnInit {
   robotDetails = [
     {
       robotimg: 'assets/robot.webp', manufacturename: 'Universal Robot',model: 'UR56',
-      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false, id:1
+      robotname: 'UR5', reach: '1.3', payload: '10', selected: false, id:1
     },
     {
       robotimg: 'assets/robot.webp', manufacturename: 'Universal Robot', model: 'UR56',
-      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false, id:2
+      robotname: 'UR5', reach: '1.3', payload: '10', selected: false, id:2
     },
     {
       robotimg: 'assets/robot.webp', manufacturename: 'Universal Robot', model: 'UR56',
-      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false, id:3
+      robotname: 'UR5', reach: '1.3', payload: '10', selected: false, id:3
     },
     {
       robotimg: 'assets/robot.webp', manufacturename: 'Universal Robot', model: 'UR56',
-      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false, id:4
+      robotname: 'UR5', reach: '1.3', payload: '10', selected: false, id:4
     },
     {
       robotimg: 'assets/robot.webp', manufacturename: 'Universal Robot', model: 'UR56',
-      robotname: 'UR5', reach: '1.3M', payload: '10Kg', selected: false, id:5
+      robotname: 'UR5', reach: '1.3', payload: '10', selected: false, id:5
     }
   ];
 
   
   ngOnInit(): void {
+    this.selectedGripperId = this.GRIPPERS[0].id
+    this.robotId = this.robotDetails && this.robotDetails.length+1 || 1;
     this.robotservice.getrobots().subscribe((data:any) => {
       // this.robotDetails=data;
+      // this.robotId = this.robotDetails && this.robotDetails.length || 1;      
       console.log(this.robotDetails)
     })
 
@@ -65,6 +74,7 @@ export class RobotComponent implements OnInit {
   }
 
   showModel(modelStatus: boolean = false) {
+    this.isEdit = false;
     this.showRobotModel = modelStatus;
   }
   showGripper(modelStatus: boolean = false) {
@@ -73,11 +83,16 @@ export class RobotComponent implements OnInit {
 
   editRobot() {
     console.log(this.selectedRobot);
+    this.isEdit = true;
     this.showRobotModel =true;
   }
 
   createRobot(event: any) {
     console.log(event);
+    // event[0] will give u the form values and event[1] will give true if Edit form false for for add form
+    this.selectedRobot = event[0];
+    this.showRobotModel = false;
+    this.showSelectGripper = true;
   }
 
   openDeleteModel(status: boolean) {
@@ -95,6 +110,15 @@ export class RobotComponent implements OnInit {
   onDeleteConfirm() {
     this.robotDetails = this.robotDetails.filter((robot: any) => robot.id !== this.selectedRobot.id);    
     this.showDeleteGripperModel = false;
+  }
+  onGripperConfirm() {
+    this.showSelectGripper = false;
+    if(this.isEdit) {
+      console.log(this.isEdit, this.selectedRobot.robotId, this.selectedGripperId)
+    } else {
+      console.log(this.isEdit, this.robotId, this.selectedGripperId)
+    }
+    
   }
 
 }
