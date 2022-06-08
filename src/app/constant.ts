27fd222590f37cbf41a9225cfdad3d13ce5708ae
Gulson Mahesh <mahesh.madasamy@wipro.com>
2022-06-08 18:12:25 +0530

    Update for Gripper selection Ui in robot page

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -1,6 +1,7 @@
 export const BASEURL = 'http://127.0.0.1:8000';
 
 export const GRIPPERS = [
-    {name:'Parallel Finger Gripper', id:1},{name:'Vaccum Gripper', id:2},{name:'Vaccum Gripper', id:3},
-    {name:'Vaccum Gripper', id:4},{name:'Vaccum Gripper', id:5},{name:'Vaccum Gripper', id:6}
+    {image: 'assets/robot.webp', name:'Parallel Finger', id:1}, {image: 'assets/robot.webp', name:'Vaccum Gripper', id:2},
+    {image: 'assets/robot.webp', name:'Vaccum Gripper', id:3}, {image: 'assets/robot.webp', name:'Vaccum Gripper', id:4},
+    {image: 'assets/robot.webp', name:'Vaccum Gripper', id:5}, {image: 'assets/robot.webp', name:'Vaccum Gripper', id:6}
 ]
---
+++ src/app/types/apptypes.ts
@@ -0,0 +1,5 @@
+export interface GripperType {
+  image: String;
+  name:String;
+  id:Number;
+}
