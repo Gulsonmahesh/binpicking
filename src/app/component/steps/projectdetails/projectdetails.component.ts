4463aa56bb2e7786d8205ae4b61df3abc9b89e8a
Gulson Mahesh <mahesh.madasamy@wipro.com>
2022-06-18 15:35:49 +0530

    update for create project

--- src/app/component/steps/projectdetails/projectdetails.component.ts
+++ src/app/component/steps/projectdetails/projectdetails.component.ts
@@ -12,6 +12,7 @@ import { UpdatesidebarService } from '../../../service/updatesidebar.service';
 export class ProjectdetailsComponent implements OnInit {
 
   model = false;
+  userDetails: any = '';
   @HostListener('document:keyup', ['$event']) closeModel(event: KeyboardEvent) {
     if (this.model && event.key == 'Escape') {
       this.model = false;
@@ -30,24 +31,27 @@ export class ProjectdetailsComponent implements OnInit {
   projectDescription = '';
 
   ngOnInit(): void {
+    if(sessionStorage.getItem('userdetails')) {
+      this.userDetails = sessionStorage.getItem('userdetails')
+    }
     // this.createprojectservice.getproject_id().subscribe((data: any) => {
     //   console.log(data)
     //   this.project_id = data.id + 1;
     //   sessionStorage.setItem('project_id', this.project_id);
     // })
+
   }
 
   createProjectDetails() {
     console.log(this.project_id, this.projectName);
-    this.sidebarService.updateSidebarStatus(0, 'completed');
-    this.routeService.movetonextpage('robot');
     const projectdetails = {
       project_name: this.projectName, description: this.projectDescription, 
-      username: "naveen", user_id: "3"
+      username: this.userDetails.username, user_id: this.userDetails.user_id
     }
     
     this.createprojectservice.saveProjectDetails(projectdetails).subscribe((data: any) => {
       if (data.status === "success") {
+        this.sidebarService.updateSidebarStatus(0, 'completed');
         this.routeService.movetonextpage('robot');
       }
       else
