5362903326740abe92af136f5b5f8a8b61dc05ce
Srevin A S <srevinias@gmail.com>
2022-06-20 17:34:55 +0530

    login page changes validation
    login page changes validation

--- src/app/component/loginpage/loginpage.component.ts
+++ src/app/component/loginpage/loginpage.component.ts
@@ -1,31 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import {  FormBuilder, FormGroup, FormControl, Validators}  from '@angular/forms';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { LoginService } from 'src/app/service/login.service';
 import { ConfirmedValidator } from './confirmed.validator';
 
-export class subject{
-  public password:any;
-  public password2:any;
+export class subject {
+  public password: any;
+  public password2: any;
 }
 @Component({
   selector: 'app-loginpage',
   templateUrl: './loginpage.component.html',
-  styleUrls: ['./loginpage.component.scss']
+  styleUrls: ['./loginpage.component.scss'],
 })
 export class LoginpageComponent implements OnInit {
   myform: any;
   userlist: any;
-  model  = new subject();
+  model = new subject();
   bntStyle: any;
   bntStyles: any;
-  numberdisabled=false;
-  maildisabled=false;
-  mailnormal=true;
-  numbernormal=true;
-  emails= false;
-  numbers=false;
+  numberdisabled = false;
+  maildisabled = false;
+  mailnormal = true;
+  numbernormal = true;
+  emails = false;
+  numbers = false;
   hide = true;
   hide1 = true;
   forgotShow: any;
@@ -34,110 +39,146 @@ export class LoginpageComponent implements OnInit {
   resetShow: any;
   afterResetSuccess: any;
   signinShow: boolean = true;
-  errvalidation=false;
+  errvalidation = false;
+  errorvalidations = false;
+  usernamesd: any;
   // isAdmin = false;
   form: FormGroup = new FormGroup({});
-  constructor(public fb: FormBuilder, public router: ActivatedRoute, public route: Router,private loginService: LoginService) {
-    this.form = fb.group({
-      password: ['', [Validators.required]],
-      confirm_password: ['', [Validators.required]]
-    }, { 
-      validator: ConfirmedValidator('password', 'confirm_password')
-    })
-   }
-   submitform(data:any){
-    if(this.model.password === this.model.password2){
+  constructor(
+    public fb: FormBuilder,
+    public router: ActivatedRoute,
+    public route: Router,
+    private loginService: LoginService
+  ) {
+    this.form = fb.group(
+      {
+        password: ['', [Validators.required]],
+        confirm_password: ['', [Validators.required]],
+      },
+      {
+        validator: ConfirmedValidator('password', 'confirm_password'),
+      }
+    );
+  }
+  submitform(data: any) {
+    if (this.model.password === this.model.password2) {
       this.otpShow = false;
       this.forgotShow = false;
       this.resetShow = false;
       this.signinShow = true;
       this.afterResetSuccess = true;
-      this.errvalidation=false;
-    }
-    else{
-      this.errvalidation=true;
+      this.errvalidation = false;
+    } else {
+      this.errvalidation = true;
     }
     // alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model, null, 4));
     // console.log(data);
     //  this.userlist= data;
-    
-      }
-   submitmail() {
-     this.numberdisabled=true;
-     this.numbernormal=false;
+  }
+  submitmail() {
+    this.numberdisabled = true;
+    this.numbernormal = false;
     this.bntStyle = 'btn-change';
   }
   submitnumber() {
-    this.maildisabled=true;
-    this.mailnormal=false;
+    this.maildisabled = true;
+    this.mailnormal = false;
     this.bntStyles = 'btn-changes';
   }
-   disableemailclick(){
+  disableemailclick() {
     // this.emails= true;
     // this.numbers=false;
   }
-  disablenumberclick(){
+  disablenumberclick() {
     // this.emails= false;
     // this.numbers=true;
   }
-   get f(){
+  get f() {
     return this.form.controls;
   }
-   
-  submit(){
+
+  submit() {
     console.log(this.form.value);
-    this.signinShow=true;
-    this.resetShow=false;
+    this.signinShow = true;
+    this.resetShow = false;
   }
-  token_data =''
+  token_data = '';
 
   ngOnInit(): void {
-    this. bntStyle = 'btn-default';
+    this.bntStyle = 'btn-default';
     this.signInForm = this.fb.group({
-      username: new FormControl('',[Validators.required, Validators.nullValidator]),
-      password: new FormControl('',[Validators.required, Validators.nullValidator]),
-      isAdmin: new FormControl(false)
-    })
+      username: new FormControl('', [
+        Validators.required,
+        Validators.nullValidator,
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.nullValidator,
+      ]),
+      isAdmin: new FormControl(false),
+    });
   }
 
   forgotPasswordClick() {
-    this.forgotShow = true;
-    this.otpShow = false;
-    this.resetShow = false;
-    this.signinShow = false;
+    if (this.usernamesd == 0) {
+      alert('please enter use name');
+    } else {
+      this.forgotShow = true;
+      this.otpShow = false;
+      this.resetShow = false;
+      this.signinShow = false;
+    }
   }
 
-  onSubmit(event:any) {
+  onSubmit(event: any) {
     event.preventDefault();
-    
-    if(this.signInForm.status === 'VALID') {
+
+    if (this.signInForm.status === 'VALID') {
       // this.route.navigate(['/gripper']);
-      const statusArray:any = ['',''];
+      const statusArray: any = ['', ''];
       sessionStorage.setItem('isAdmin', this.signInForm.controls.isAdmin.value);
       sessionStorage.setItem('sidebarstatus', JSON.stringify(statusArray));
-      if(this.signInForm.controls.isAdmin.value === true) {
+      if (this.signInForm.controls.isAdmin.value === true) {
         // this.operator = "admin"
         const logindetails = new FormData();
-        logindetails.append("username", <string>this.signInForm.controls.username.value );
-        logindetails.append("password", <string>this.signInForm.controls.password.value );
-        console.log(this.signInForm.value)
-        this.loginService.loginDetails(logindetails).subscribe((data:any) => {
-          console.log("data",data)
-          sessionStorage.setItem('userdetails',JSON.stringify(data.username, data.user_id));
+        logindetails.append(
+          'username',
+          <string>this.signInForm.controls.username.value
+        );
+        logindetails.append(
+          'password',
+          <string>this.signInForm.controls.password.value
+        );
+        console.log(this.signInForm.value);
+        this.loginService.loginDetails(logindetails).subscribe((data: any) => {
+          console.log('data', data);
+          sessionStorage.setItem(
+            'userdetails',
+            JSON.stringify(data.username, data.user_id)
+          );
           this.route.navigate(['/gripper']);
-        })
-      // this.route.navigate(['/gripper']);
+        });
+        // this.route.navigate(['/gripper']);
       } else {
         // this.operator = "user"
-        const statusArray:any = ['','','','','','','','','',''];
+        const statusArray: any = ['', '', '', '', '', '', '', '', '', ''];
         sessionStorage.setItem('sidebarstatus', JSON.stringify(statusArray));
-        console.log(this.signInForm.value)
+        console.log(this.signInForm.value);
         const logindetails = new FormData();
-        logindetails.append("username", <string>this.signInForm.controls.username.value );
-        logindetails.append("password", <string>this.signInForm.controls.password.value );
-        this.loginService.loginDetails(logindetails).subscribe((data:any) => {
-          console.log("data",data)
-          sessionStorage.setItem('userdetails',JSON.stringify(data.username, data.user_id));
+        logindetails.append(
+          'username',
+          <string>this.signInForm.controls.username.value
+        );
+        logindetails.append(
+          'password',
+          <string>this.signInForm.controls.password.value
+        );
+        this.loginService.loginDetails(logindetails).subscribe((data: any) => {
+          console.log('data', data);
+          sessionStorage.setItem(
+            'userdetails',
+            JSON.stringify(data.username, data.user_id)
+          );
           // this.loginService.UserDetails().subscribe((new_data:any) => {
           //  console.log(new_data)
           // });
@@ -154,10 +195,10 @@ export class LoginpageComponent implements OnInit {
     this.signinShow = false;
     // this.route.navigate(['/project-details']);
   }
-  openverify(){
-    this.otpShow=false;
-    this.resetShow= true;
-    this.forgotShow=false;
+  openverify() {
+    this.otpShow = false;
+    this.resetShow = true;
+    this.forgotShow = false;
   }
   successOtp() {
     this.otpShow = true;
@@ -165,9 +206,9 @@ export class LoginpageComponent implements OnInit {
     this.resetShow = false;
     this.signinShow = false;
   }
-  verifiedotp(){
-    this.otpShow= false;
-    this.resetShow= true;
+  verifiedotp() {
+    this.otpShow = false;
+    this.resetShow = true;
   }
 
   isLoadingfal: boolean = true;
@@ -180,7 +221,7 @@ export class LoginpageComponent implements OnInit {
     setTimeout(() => {
       this.isLoading = false;
       this.isLoadingTrue = true;
-    }, 1000)
+    }, 1000);
 
     // this.isLoading= false;
   }
@@ -197,5 +238,4 @@ export class LoginpageComponent implements OnInit {
   setAdmin() {
     // console.log(this.isAdmin)
   }
-
 }
